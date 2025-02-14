a0ab604461b92393666abc60e886d3b66f476a87
KarenDC5 <karen.duran@iteso.mx>
2025-02-14 14:59:29 -0600

    primer intento

--- uni-dec-cent.py
+++ uni-dec-cent.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Unidades, decenas y centenas que tiene un número 
+Karen Durán Cárdenas 
+757136
+Ingeniería Civil 
+Algoritmos y Programación 
+ESI232B2
+14/02/25
+10 min
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = float(input("Introduzca un número: "))
 
 # Proceso
-
+centenas = numero//100
+decenas = (numero - (100*centenas))//10
+unidades = numero - 100*centenas - 10*decenas 
 
 # Salidas
-print(salida)
+print("Centenas: ", centenas)
+print("Decenas: ", decenas)
+print("Unidades: ", unidades)
